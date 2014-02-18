14853c83d699f232d1284106407b7e4e86f91dc1
Alycia Moncrieff <moncrieffa@gmail.com>
2014-02-18 12:14:28 -0800

    fix mistake with to email. Should be recipient_email to grab email address from options array.

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,7 +2,7 @@ class ApplicationMailer < ActionMailer::Base
   def contact_form_message(message)
     @message = message
     mail :subject => @message.subject,
-         :to      => @message.to,
+         :to      => @message.recipient_email,
          :from    => "#{@message.name} <#{@message.email}>"
   end
 end
